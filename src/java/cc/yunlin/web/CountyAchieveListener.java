62afcdfb7d9a4baf7d1db47dd6887eeaff26ec17
sinsnn <sinsnn.lin@gmail.com>
2017-04-15 11:28:05 +0800

    修正 index.html

---
+++ src/java/cc/yunlin/web/CountyAchieveListener.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.yunlin.web;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author sinsnn
+ */
+@WebListener
+public class CountyAchieveListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context)
+                           initContext.lookup("java:/comp/env");
+            DataSource dataSource = (DataSource) envContext.lookup("jdbc/CountyAchieve");
+            ServletContext context = sce.getServletContext();
+//下面需新增 model 的壹些服務類別
+            
+
+        } catch (NamingException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
